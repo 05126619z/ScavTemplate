2868c4ebc42057a8f4bcdf06c9ed1c23236e4235
05126619z <05126619z@gmail.com>
2026-01-16 12:59:47 -0300

    Created this template

---
+++ ScavTemplate/Patches.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace ModNamespace
+{
+    internal class Patches
+    {
+        [HarmonyPatch(typeof(Body))]
+        internal static class BodyPatch
+        {
+            [HarmonyPatch(nameof(Body.Start))]
+            [HarmonyPostfix]
+            private static void StartPatch()
+            {
+                ConsoleScript.instance.LogToConsole("Hello World!");
+            }
+        }
+    }
+}
---
+++ ScavTemplate/Plugin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using MonoMod.RuntimeDetour;
+using UnityEngine;
+
+namespace ModNamespace
+{
+    [BepInPlugin("username.modname", "ModName", "0.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static new ManualLogSource Logger;
+        private readonly Harmony _harmony = new("username.modname");
+        public static Plugin Instance { get; private set; } = null!;
+
+        public void Awake()
+        {
+            Logger = base.Logger;
+            Instance = this;
+
+            _harmony.PatchAll();
+            Logger.LogInfo($"Plugin ModName is loaded!");
+        }
+    }
+}
